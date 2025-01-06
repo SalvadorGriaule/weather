242115e0f245c45e5cff5f3f62bdc32a56c0d9dd
SalvadorGriaule <morales.quentin@gmx.fr>
2025-01-06 16:18:27 +0100

    update in 16:18 06/01

--- weather-front/src/asset/Componants/geolocalisation.js
+++ weather-front/src/asset/Componants/geolocalisation.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Weather from "./weather";
 
 function Position(props) {
--- weather-front/src/asset/Componants/weather.js
+++ weather-front/src/asset/Componants/weather.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Weather(props) {
     const [weatherRes, setWeatherRes] = useState(null)
@@ -6,42 +6,33 @@ function Weather(props) {
     let today = new Date();
     let day = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
     let month = today.getMonth() + 1 < 10 ? "0" + (today.getMonth() + 1) : (today.getMonth() + 1);
-    let time = today.getFullYear() + "-" + month + "-" + day + "T" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "Z";
+    let time = today.getFullYear() + "-" + month + "-" + day + "T" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 
     async function fetchWeather() {
         try {
-            const resp = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${props.latitude}&longitude=${props.longitude}&current=temperature_2m,precipitation,wind_speed_10m`, {
+            const resp = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${props.latitude}&longitude=${props.longitude}&current=temperature_2m,precipitation,wind_speed_10m,relative_humidity_2m`, {
                 mode: "cors",
                 credentials: "include"
             }).then(val => val.json());
             setWeatherRes(resp.current);
-            console.log(weatherRes);
         } catch (error) {
             console.log(error);
         }
     }
 
     if (weatherRes == null) fetchWeather();
-
+    
     return (
         <div>
-            <p>{time}</p>
+            <p>temps réel: {time}</p>
             {weatherRes && (
                 <div>
-                    <p>{Object.entries(weatherRes)}</p>
-                    {Object.entries(weatherRes).map((data) => {
-                        <p>{data}</p>
-                    })}
+                    {Object.entries(weatherRes).map((data) => (
+                        <p>{data[0]} : {data[1]}</p>
+                    ))}
                 </div>
             )}
-            {/* {weatherRes && (
-                    <div>
-                    {weatherRes.map((data) => {
-                        <div>{data}</div>
-                    })}
-                    </div>
-            )} */}
         </div>
     )
 }
